c846a1bc001dae89a418ea0c8a29c4a67797a6ea
manav755 <manavbhatt1343@gmail.com>
2026-08-21 15:31:19 +0530

    Add files via upload
    Improved code

--- practical2.py
+++ practical2.py
@@ -1,13 +1,20 @@
+#Missing Roll Number
+
 list_num = []
 
-num = int(input("Enter Range for numbers : "))
+num = int(input("Enter Range of numbers : "))
 
 for i in range(num):
-    list_num.append(int(input("Enter NUmbers : ")))
+    n = int(input("Enter Number : "))
+    if (n <= num):
+        list_num.append(n)
+    else:
+        print("Please enter number under given range!!")
 
 
 print(list_num)
 
-for i in range(max(list_num) - 1):
+print("Missing Numbers :-")
+for i in range(1, max(list_num) - 1):
     if i not in list_num:
         print(i)
